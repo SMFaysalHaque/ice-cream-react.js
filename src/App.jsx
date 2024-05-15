a7eca020ebef3397314785be5e089178111f5a27
SMFaysalHaque <smfaysalhaque007@gmail.com>
2024-05-15 18:49:08 +0600

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Body from './components/Body/Body.jsx';
+import Footer from './components/Footer/Footer.jsx';
+import Header from './components/Header/Header.jsx';
+import Layout from './components/Layout/Layout.jsx';
+
+function App() {
+    return (
+        <Layout>
+            <Header />
+            <Body />
+            <Footer />
+        </Layout>
+    );
+}
+
+export default App;
